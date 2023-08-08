0b0310418b65621ee35dc0d59376e113a6a7d6c1
Robson R Silva <robson_azaleia@hotmail.com>
2023-08-08 14:46:27 -0300

    Update main.py
    Comentários adicionados

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-vendatotal = float(input('digite valor total:'))
-boletos = float(input('digite valor dos boletos: '))
-salarios = float(input('digite valor dos salarios: '))
-gastosloja = float(input('digite valor dos gastos: '))
-aluguel = float(input('digite o valor so aluguel: '))
+# Solicita ao usuário os valores relacionados às finanças da loja
+vendatotal = float(input('Digite o valor total das vendas: '))
+boletos = float(input('Digite o valor dos boletos: '))
+salarios = float(input('Digite o valor dos salários: '))
+gastosloja = float(input('Digite o valor dos gastos da loja: '))
+aluguel = float(input('Digite o valor do aluguel: '))
 
+# Calcula a soma do saldo disponível após dedução dos gastos
 soma = vendatotal - (boletos + salarios + gastosloja + aluguel)
 
+# Calcula as porcentagens de cada gasto em relação ao valor total das vendas
 porcentagem_boletos = (boletos / vendatotal) * 100
 porcentagem_salarios = (salarios / vendatotal) * 100
 porcentagem_gastosloja = (gastosloja / vendatotal) * 100
 porcentagem_aluguel = (aluguel / vendatotal) * 100
 
-print('O valor de fechamento de caixa é de {}.'.format(s))
-print(f'O valor dos boletos equivale a {porcentagem_boletos :.2f}% do valor total das vendas')
-print(f'O valor dos salarios equivale a {porcentagem_salarios :.2f}% do valor total das vendas')
-print(f'O valor dos gastos da loja equivalem a {porcentagem_gastosloja :.2f} do valor total das vendas')
-print(f'O valor do aluguel equivale a {porcentagem_aluguel :.2f}% do valor total das vendas')
+# Exibe o valor de fechamento de caixa após dedução dos gastos
+print('O valor de fechamento de caixa é de {}.'.format(soma))
+
+# Exibe as porcentagens dos gastos em relação ao valor total das vendas
+print(f'O valor dos boletos equivale a {porcentagem_boletos:.2f}% do valor total das vendas')
+print(f'O valor dos salários equivale a {porcentagem_salarios:.2f}% do valor total das vendas')
+print(f'O valor dos gastos da loja equivale a {porcentagem_gastosloja:.2f}% do valor total das vendas')
+print(f'O valor do aluguel equivale a {porcentagem_aluguel:.2f}% do valor total das vendas')
